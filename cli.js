8c8ba2e97eff5de858480ce68fe008fb7a66c414
振震 <865702942@qq.com>
2016-01-15 12:47:01 +0800

    server init

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node --harmony
+'use strict';
+
+var program = require('commander');
+
+program
+  .version(require('./package').version, '-v, --version')
+  .option('-p, --port <port>', 'port')
+  .option('--https', 'https')
+  .parse(process.argv);
+
+program.cwd = process.cwd();
+require('./lib/server')(program);
